322fde622f3a3dd3e450ced5e60b5ed76327d09c
Sam Carroll <sam.carroll@student.scots.college>
2022-08-15 22:31:30 +1000

    combined into main.py

--- main.py
+++ main.py
@@ -95,11 +95,10 @@ class Game:
     def clear_term():
         os.system('cls||clear')
     
-        
     def init_pygame(self):
         pygame.init()
         self.window = pygame.display.set_mode((self.WIDTH, self.HEIGHT), 0, 32)
-        pygame.display.set_caption('Pygame Tile Based RPG Example')
+        pygame.display.set_caption('Monster Hunter \'86')
         self.clock = pygame.time.Clock()
     
     def get_font(self, path, height_divisor):
@@ -113,6 +112,207 @@ class Game:
         self.TIMING_LAST_MSG = new
 
     
+    pass
+
+class Character: 
+    
+    def __init__(self, surf, row, col):
+        self.surf = surf
+        self.row = row
+        self.col = col
+        pass
+    
+    def printInfo(self):
+        healthText = BUTTON_FONT.render(
+            f'HP: {playerStats["health"]}', 1, BLACK)
+        self.surf.blit(healthText, (self.col * box_size + (box_size/8), self.row * box_size - 2*(box_size/3)))
+
+    def getCharIm(pathn):
+        return pygame.transform.scale(
+            pygame.image.load(
+                os.path.join(IMAGES_FOLDER, pathn)
+            ), (box_size/(3/2),box_size*(4/3))
+        )
+    
+    pass
+
+class Player(Character):
+    
+    playerStats = {
+        "health": 20
+    }
+    # images = "players": {
+    #     "dude-d": getCharIm("dude-d.png"),
+    #     "dude-u": getCharIm("dude-u.png"),
+    #     "dude-r": getCharIm("dude-r.png"),
+    #     "dude-l": getCharIm("dude-l.png"),
+    
+    def __init__(self, row, col, surf, name, path):
+        super().__init__(row, col, surf)
+        self.name = name
+        self.path = path
+        
+    def move(self, dir):
+        if dir == "left":
+            self.path = constants.images["players"]["dude-l"]
+            if self.col -1 >= 0 and item_grid[self.row][self.col-1] == 0 and map_grid[self.row][self.col-1].passable:
+                self.col -= 1
+                self.checkActions(map_grid[self.row][self.col])
+        elif dir == "right":
+            self.path = constants.images["players"]["dude-r"]
+            if self.col + 1 < grid_x and item_grid[self.row][self.col+1] == 0 and map_grid[self.row][self.col+1].passable:
+                self.col += 1
+                self.checkActions(map_grid[self.row][self.col])
+        elif dir == "up":
+            self.path = constants.images["players"]["dude-u"]
+            if self.row - 1 >= 0 and item_grid[self.row-1][self.col] == 0 and map_grid[self.row-1][self.col].passable:
+                self.row -= 1
+                self.checkActions(map_grid[self.row][self.col])
+        elif dir == "down":
+            self.path = constants.images["players"]["dude-d"]
+            if self.row + 1 < grid_y and item_grid[self.row + 1][self.col] == 0 and map_grid[self.row+1][self.col].passable:
+                self.row += 1
+                self.checkActions(map_grid[self.row][self.col])
+    
+    def draw(self):
+        window.blit(self.path, (self.col * box_size + (box_size/4), self.row * box_size - (box_size/3)))
+           
+    pass
+
+class Monster(Character):
+    def __init__(self, row, col, name, color, path, surf):
+        super().__init__(row, col, surf)
+        self.name = name
+        self.color = color
+        self.path = path
+        
+    # def draw(self):
+    # pygame.draw.rect(
+    #   self.surf, 
+    #   self.color, 
+    #   pygame.Rect(
+    #     self.col * box_size + (box_size/3), 
+    #     self.row * box_size + (box_size/3), 
+    #     item_size, item_size
+    #   )
+    # )
+    # window.blit(self.path, (self.col * box_size + (box_size/4), self.row * box_size - (box_size/3),))
+        
+    pass
+
+class Skeleton(Monster):
+    
+    def __init__(self):
+        super().__init__()
+        
+    pass
+
+class Enderman(Monster):
+    
+    def __init__(self):
+        super().__init__()
+        
+    pass
+
+class Map():
+    
+    def __init__(self, map_path, surf):
+        self._map_path = map_path
+        # self._map_width = map_width
+        # self._map_height = map_height 
+        self.surf = surf # pygame surface 
+    
+    def getMap(self):
+        curMap = open(os.path.join(MAPS_FOLDER,selected_map), "r")
+
+        # Read in the map file
+        for line in curMap:
+            line = line.strip().split(",")
+            if line != ['']:
+                
+                # if len(line) >= 4:
+                #     special = line[3].strip().split('|')
+                # else:
+                #     special = None
+                    
+                tile = Tile(r, c, BLACK, images["tiles"][selected_map][line[0]], eval(line[1]), line[2], window)
+                # tile = Tile(r, c, colors[line[0]], images["tiles"][line[0]], eval(line[1]), line[2], window, special)
+        
+                if line[2] != "none":
+                    item = line[2].split("|")
+                    if item[0] == "monster":
+                        monster = Monster(r, c, item[0], None, images["monsters"][item[1]], window)
+                        temp_item_grid.append(monster)
+                    elif item[0] == "player":
+                        dude.row = r
+                        dude.col = c
+                        temp_item_grid.append(0)
+                else:
+                    temp_item_grid.append(0)
+
+                temp_grid.append(tile)
+                c += 1
+
+                if c % grid_x == 0:
+                    r += 1
+                    c = 0
+                    map_grid.append(temp_grid)
+                    item_grid.append(temp_item_grid)
+                    temp_grid = []
+                    temp_item_grid = []
+
+        curMap.close()
+    
+    pass
+
+class Tile(Map):
+    
+    def __init__(self, row, col, tile_set, path, passable, item, surf):
+        super().__init__(surf)
+        self.col = col
+        self.row = row
+        self.tile_set = tile_set
+        self.path = path
+        self.passable = passable
+        self.item = item
+        
+    def getTileIm(pathn):
+        return pygame.transform.scale(
+            pygame.image.load(
+                os.path.join(IMAGES_FOLDER, pathn)
+            ), (box_size, box_size)
+    )
+    
+
+    def draw(self):
+        pygame.draw.rect(
+            self.surf, 
+            self.tile_set, 
+            pygame.Rect(self.col * (box_size), self.row * (box_size), box_size, box_size)
+        )
+        window.blit(self.path, (self.col * (box_size), self.row * (box_size)))
+        
+    pass
+
+class SpecialTile(Tile):
+    
+    def __init__(self):
+        super().__init__()
+        
+    pass
+
+class TeleportTile(SpecialTile):
+    
+    def __init__(self):
+        super().__init__()
+        
+    pass
+
+class ChangeMapTile(Tile):
+    
+    def __init__(self):
+        super().__init__()
+        
     pass
 
 game = Game()
