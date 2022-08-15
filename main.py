4bd10ff7d2a3e670f6fba6428d82ff7d0e8277f5
Sam Carroll <sam.carroll@student.scots.college>
2022-08-16 03:07:35 +1000

    THE MAP DRAWS!

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 # v4
 
-from urllib.parse import MAX_CACHE_SIZE
+from email.mime import image
 import pygame
 import os
 import json
-from pprint import pprint
-
+import sys
 
 class Game():
     def __init__(self):
-        self.map = Map("overworld_1")
-        self.player = Player("Danny")
-        self.enemies = []
-
-        print(self)
-        pass
+        self.setup_pygame()
 
     def start(self):
-        self.setup_pygame()
+        self.map = Map("overworld_1")
+        self.player = Player("Danny")
+        self.monsters = []
+
         self.main_loop()
 
     def setup_pygame(self):
@@ -26,7 +23,7 @@ class Game():
         self.window_size = (infoObject.current_w // 2,
                             infoObject.current_h // 2)
         self.window = pygame.display.set_mode(
-            (self.window_size[0], self.window_size[1]), 0, 32)
+            (self.window_size[0], self.window_size[1]), pygame.RESIZABLE, 32)
         pygame.display.set_caption('Monster Hunter \'86')
         self.clock = pygame.time.Clock()
         self.running = True
@@ -34,15 +31,30 @@ class Game():
     def main_loop(self):
         while self.running:
 
-            self.map.draw()
-            self.player.draw()
-            self.monsters.draw()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
+                # if event.type == pygame.KEYDOWN:
+                #     if event.key == pygame.K_LEFT or event.key == ord('a'):
+                #         dude.move("left")
+                #     if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                #         dude.move("right")
+                #     if event.key == pygame.K_UP or event.key == ord('w'):
+                #         dude.move("up")
+                #     if event.key == pygame.K_DOWN or event.key == ord('s'):
+                #         dude.move("down")
+
+            self.map.draw(self.window)
+            # self.player.draw(self.window)
+            # for monster in self.monsters:
+            #     monster.draw(self.window)
             pygame.display.update()
-            self.clock.tick()
+            self.clock.tick(30)
 
     def __repr__(self):
-        return "Game<{}, {}, {}e>".format(self.map, self.player, len(self.enemies))
+        return "Game<{}, {}, {}m>".format(self.map, self.player, len(self.enemies))
 
 
 class Map():
@@ -55,6 +67,12 @@ class Map():
         self.load_map()
 
         print(self)
+
+    def load_tilesets(self):
+        self.tilesets = []
+        for tileset in reversed(self.map_data["tilesets"]):
+            self.tilesets.append(
+                (tileset["firstgid"], Tileset(tileset["source"], self.filepath)))
 
     def load_map(self):
         self.map = []
@@ -69,27 +87,30 @@ class Map():
         # Add in the actual tile layers to each Tile object
         for layer_num, layer_object in enumerate(self.map_data["layers"]):
             if layer_object["visible"]:
-                for tile_id, tileset_num in enumerate(layer_object["data"]):
+                for tileset_num, map_tile_id in enumerate(layer_object["data"]):
 
-                    row = tile_id // layer_object["width"] + layer_object["y"]
-                    col = tile_id % layer_object["width"] + layer_object["x"]
+                    row = tileset_num // layer_object["width"] + \
+                        layer_object["y"]
+                    col = tileset_num % layer_object["width"] + \
+                        layer_object["x"]
 
-                    self.map[row][col].add_layer(self.get_tile_image(tile_id))
+                    self.map[row][col].add_layer(
+                        self.get_tile_surface(map_tile_id), layer_object["name"])
 
-    def load_tilesets(self):
-        self.tilesets = []
-        for tileset in self.map_data["tilesets"]:
-            self.tilesets.append((tileset["firstgid"], Tileset(tileset["source"])))
-            pass
+    def get_tile_surface(self, map_tile_id):
+        for tileset_tuple in self.tilesets:
+            if tileset_tuple[0] <= map_tile_id:
+                local_tile_id = map_tile_id - tileset_tuple[0]
+                return tileset_tuple[1].get_tile_surface(local_tile_id)
 
-    def get_tile_image(self, tile_id):
-
-        pass
-
-    def draw(self):
-        for row_num, row_tiles in enumerate(self.tiles):
-            for col_num, col_tile in enumerate(row_tiles):
-                col_tile.draw()
+    def draw(self, window):
+        for row_tiles in self.map:
+            for col_tile in row_tiles:
+                col_tile.draw(
+                    window,
+                    self.map_data["tilewidth"],
+                    self.map_data["tileheight"]
+                )
 
     def __repr__(self) -> str:
         return "Map<{}, {}w, {}h>".format(self.name, self.map_data["width"], self.map_data["height"])
@@ -102,13 +123,20 @@ class Tile():
         self.layers = []
         pass
 
-    def add_layer(self, image, layer_name):
+    def add_layer(self, image_surface, layer_name):
+        if image_surface == None:
+            return
+
         self.layers.append(
-            (image, layer_name)
+            (image_surface, layer_name)
         )
 
-    def draw(self):
-
+    def draw(self, window, width, height):
+        for layer in self.layers:
+            window.blit(
+                layer[0],
+                (self.col * width, self.row * height)
+            )
         pass
 
     def __repr__(self):
@@ -121,6 +149,9 @@ class Character():
         self.max_health = 100
         self.health = self.max_health
         self.level = 1
+
+    def draw(self):
+        pass
 
     def __repr__(self):
         return type(self).__name__ + "<{}, {}HP, LVL{}>".format(self.name, self.health, self.level)
@@ -137,14 +168,16 @@ class Player(Character):
 
 
 class Tileset():
-    def __init__(self, tsx_path) -> None:
-        self.filepath = tsx_path.replace(".tsx", ".tsj")
+    def __init__(self, tsx_path, map_path) -> None:
+        self.filepath = os.path.join(os.path.dirname(
+            map_path), tsx_path.replace(".tsx", ".tsj"))
         self.tileset_data = json.load(open(self.filepath, "r"))
 
         # Get the image file using the same directory
-        self.sheet = pygame.image.load(
-            os.path.join(os.path.dirname(self.filepath),
-                         self.tileset_data["image"])).convert()
+        self.sheetpath = os.path.join(os.path.dirname(
+            self.filepath), self.tileset_data["image"])
+        self.sheet = pygame.image.load(self.sheetpath).convert_alpha()
+
 
         self.tile_collisions = []
         self.load_collisions()
@@ -170,16 +203,20 @@ class Tileset():
             self.tileset_data["tileheight"]
         )
 
-    def get_tile_surface(self, rectangle):
+    def get_tile_surface(self, local_tile_id):
+        rectangle = self.get_tile_rect(local_tile_id)
         # From https://www.pygame.org/wiki/Spritesheet
         rect = pygame.Rect(rectangle)
-        image = pygame.Surface(rect.size).convert()
+        image = pygame.Surface(rect.size, pygame.SRCALPHA, 32).convert_alpha()
         image.blit(self.sheet, (0, 0), rect)
         # if colorkey is not None:
         #     if colorkey is -1:
         #         colorkey = image.get_at((0,0))
         #     image.set_colorkey(colorkey, pygame.RLEACCEL)
         return image
+
+    def __repr__(self):
+        return type(self).__name__ + "<{}, {}>".format(os.path.basename(self.filepath), os.path.basename(self.sheetpath))
 
 
 class Utils():
